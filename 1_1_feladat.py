ff93fcfa998aa893fc115d6a7e051f38751c346f
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2024-11-11 10:24:40 +0100

    1. Solution

--- 1_1_feladat.py
+++ 1_1_feladat.py
@@ -3,6 +3,10 @@
 Készíts egy programot, amely a felhasználótól keresztneveket kér be egészen addig, amíg az ENTER-t nem üt (nem ad meg újabb nevet a bekérésnél)! A program a bekért neveket írja ki a képernyőre!
 """
 
-folytat = True
-while folytat:
-    nev = input("")
+nevek = []
+while True:
+    nev = input("Adj meg egy keresztnevet: ")
+    if nev == "":
+        break
+    nevek.append(nev)
+print(nevek)
